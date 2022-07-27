737abaee5918255aad92334182e6a4be85eb8f5f
cimmentu <cimmentumarriu@gmail.com>
2022-07-27 08:01:21 +0200

    improvements

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using Servidor.Logic;
 using Servidor.Model;
 
@@ -6,13 +8,39 @@ namespace Servidor
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Servidor!");
             RequestMaker maker = new RequestMaker();
             RequestQueue requestQ = maker.MakeRequests();
 
-            while(requestQ.)
+            int age = 0;
+
+            while(true)
+            {
+                requestQ.PrintStatus();
+                if(requestQ.Count > 0)
+                {
+                    IRequest lowReq = requestQ.FirstOrDefault(x => x.Priority == Priority.Low);
+                    if(lowReq != null)
+                    {
+                        if(age > 3)
+                        {
+                            lowReq.Priority = Priority.High;
+                            age = 0;
+                        }
+                        else
+                        {
+                            age++;
+                        }
+                    }
+                    requestQ.SortByPriority(); //TBD
+                    IRequest req = requestQ.Pop();
+                    req.Process();
+                }
+                Thread.Sleep(2000);
+            }
 
         }
     }
